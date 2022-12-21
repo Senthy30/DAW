cb85f9f2feac53a44e32e716de92a2afd03eacdb
Senthy30 <denis-florin.cringanu@s.unibuc.ro>
2022-12-21 22:27:18 +0200

    update

--- index.php
+++ index.php
@@ -1,26 +1,133 @@
-<?php session_start(); ?>
+<?php session_start(); include "db_conn.php";?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Home</title>
+
+		<link rel="stylesheet" href="static/css/style.css">
 	</head>
 
 	<body>
 
-		<?php if(isset($_SESSION['id']) && isset($_SESSION['email'])) { ?>
+		<div class="content">
 
-			<h1>Hello, <?php echo $_SESSION['fname']; ?>!</h1>
-			<a href="login/logout.php">Logout</a>
+			<div class="topPage">
+				<img src="img/bkImage.png" class="bkImage">
 
-		<?php } else { ?>
+				<div class="topMenu">
 
-			<h1>Hello, Guest!</h1>
+					<div class="logo">
+						Hotel Maria
+					</div>
 
-			<a href="login/index.php">Log in</a> <br>
-			<a href="register/index.php">Create an account</a>
+					<div class="optionsMenu">
+						<?php 
+							if(isset($_SESSION['id']) && isset($_SESSION['email'])){
+						?>	
+							<div class="menuButton" onclick="window.location.href = 'login/logout.php'">
+								<div class="textButton">
+									Profile
+								</div>
+							</div>
+						<?php 
+							} else {
+						?>
+							<div class="menuButton" onclick="window.location.href = 'login/index.php'">
+								<div class="textButton">
+									Sign in
+								</div>
+							</div>
 
-		<?php } ?>
+							<div class="menuButton" onclick="window.location.href = 'register/index.php'">
+								<div class="textButton">
+									Register
+								</div>
+							</div>
+						<?php
+							}
+						?>
+
+						<?php 
+							$idUser = -1;
+							if(isset($_SESSION['id']) && isset($_SESSION['email']))
+								$idUser = $_SESSION['id'];
+							$sql = "SELECT isAdmin FROM permission WHERE id_user = '$idUser';";
+							$result = mysqli_query($conn, $sql);
+							$result = $result->fetch_object();
+
+							if($idUser != -1 && $result->isAdmin == 1){ 
+						?>
+								<div class="menuButtonAdmin">
+									<div class="textButtonAdmin">
+										Admin
+									</div>
+								</div>
+						<?php 
+							} 
+						?>
+
+						<div class="language">
+							<img src="img/flagRo.png" class="flagLang">
+						</div>
+
+						<div class="currency">
+							RON
+						</div>
+						
+					</div>
+
+				</div>
+
+				<div class="aboutYou">
+					<div class="motto">
+						Don’t just dream of a vacation. Just do it!
+					</div>
+
+					<div class="searchMotto">
+						Search deals that fit you.
+					</div>
+				</div>
+
+				<div class="searchMenu">
+					<div class="typeInput">
+						<input type="text" placeholder="Type of room">
+					</div>
+
+					<div class="checkInOut">
+						<div>
+							Check-in
+						</div>
+
+						<input type="date">
+					</div>
+
+					<div class="checkInOut">
+						<div>
+							Check-out
+						</div>
+
+						<input type="date">
+					</div>
+
+					<div class="numberInput">
+						<div>
+							People
+						</div>
+
+						<input type="number" min="0">
+					</div>
+
+					<div class="submitInput">
+						<div>
+							Submit
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
 
 	</body>
 </html>
