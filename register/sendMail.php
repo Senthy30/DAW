381c310f784c82ae73de0cc595f2ab7c1e2bddb2
Senthy30 <denis-florin.cringanu@s.unibuc.ro>
2022-12-22 20:49:26 +0200

    update

--- register/sendMail.php
+++ register/sendMail.php
@@ -31,6 +31,11 @@
         $response = curl_exec($curl);
         $err = curl_error($curl);
         curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
     }
 
     sendMailForAccountConfirmation("denisflorin69@yahoo.com");
